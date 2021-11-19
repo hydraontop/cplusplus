c3adc85262bc65ed5c07ee5af34e87abce5bcdd5
hydraontopp#0009 <90989258+hydraontop@users.noreply.github.com>
2021-11-19 17:28:49 -0500

    Update poop.py

--- poop.py
+++ poop.py
@@ -1,1 +1,12 @@
+import asyncio
+import socket
+import warnings
+from collections import defaultdict
+from contextlib import suppress
+from typing import Dict, List, Optional
 
+import aiohttp
+import discord
+from async_rediscache import RedisSession
+from discord.ext import commands
+from sentry_sdk import push_scope
